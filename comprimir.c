cda47d9e02fe2e64d23cf88a191b2e76575e44ad
diegoelguetar-cmyk <diego.elgueta.r@ug.uchile.cl>
2026-08-20 22:20:47 -0400

    primera versión

--- comprimir.c
+++ comprimir.c
@@ -8,15 +8,19 @@
 typedef unsigned int unit;
 
 int det_nbits(unit a[], int n_bit){
+  """
+  función que determina el numero en que podemos truncar, asegurando que no se exceda de los 32 bits de un uint. 
+  también retorna el tamaño del arreglo a[].
+  """
   int l = sizeof(a)/4;
   int n = 32; // numero de bits de unit
 
   if (n_bit*l > 32){
     n_bit = 32/l; // será un uint, pues son todas variables de tipo unit
-    return n_bit;
+    return {n_bit, l};
   }
   else{
-    return n_bit;
+    return {n_bit, l};
   }
 }
 
@@ -40,10 +44,27 @@ int truncar(int n_bit, int e){
   return t;
 }
 
+int concatenar(int a[], int nbits, int l){
+  """
+  Función que concatena los valores de a[] en un solo valor.
+  """
+  int e = 0;
+  for (int i =0; i<l; i++){
+    e |= a[i]; // usamos el operador O para unir los valores 
+  }
+  return e;
+}
+
 uint comprimir(uint a[], int nbits) {
-  nbits = det_nbits(a, nbits);
+  nbits, l = det_nbits(a, nbits);
   int mask = mask(nbits);
-  
+
+  for (int i =0; i<l; i++){
+    a[i] = truncar(nbits, a[i]);
+    a[i] = a[i] << (i*nbits);
+  }
+
+
 }
 
 
