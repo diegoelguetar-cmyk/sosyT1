6baedde99b10b52fe75d991af0866b55d50f132b
diegoelguetar-cmyk <diego.elgueta.r@ug.uchile.cl>
2026-08-20 22:35:42 -0400

    arreglos 3

--- comprimir.c
+++ comprimir.c
@@ -7,28 +7,34 @@
 
 typedef unsigned int unit;
 
-int det_nbits(unit a[], int n_bit){
-  """
+int len(unit a[]){
+  return sizeof(a)>>2;
+}
+
+int det_nbits(unit a[], int n_bit){  
+  /*
   función que determina el numero en que podemos truncar, asegurando que no se exceda de los 32 bits de un uint. 
-  también retorna el tamaño del arreglo a[].
-  """
-  int l = sizeof(a)/4;
+
+  */
+
+  int l = len(a);
   int n = 32; // numero de bits de unit
 
   if (n_bit*l > 32){
     n_bit = 32/l; // será un uint, pues son todas variables de tipo unit
-    return {n_bit, l};
+    return n_bit;
   }
   else{
-    return {n_bit, l};
+    return n_bit;
   }
 }
 
 int mask(int n_bit){
-  """
+  /*
   Función que retorna una mascara para truncar un número a n_bit bits.
   Dado el n_bit, la función se asegura que no se exceda de los 2 byts de unit
-  """
+  */
+  
 
   int m = -1;
   int mask = ~(m << n_bit);
@@ -36,19 +42,19 @@ int mask(int n_bit){
 }
 
 int truncar(int n_bit, int e){
-  """
-  Función que trunca el valor a n_bit. 
-  """
+ /*
+ Función que trunca el valor a n_bit. 
+ */
   int m = mask(n_bit);
   int t = e & m;
   return t;
 }
 
 int concatenar(int a[], int nbits, int l){
-  """
+  /*
   Función que concatena los valores de a[] en un solo valor.
-  """
-  int e = 0;
+  */
+    int e = 0;
   for (int i =0; i<l; i++){
     e |= a[i]; // usamos el operador O para unir los valores 
   }
@@ -56,9 +62,9 @@ int concatenar(int a[], int nbits, int l){
 }
 
 uint comprimir(uint a[], int nbits) {
-  {nbits, l} = det_nbits(a, nbits);
-  int mask = mask(nbits);
-
+  int l = len(a); 
+  nbits= det_nbits(a, nbits);
+  
   for (int i =0; i<l; i++){
     a[i] = truncar(nbits, a[i]);
     a[i] = a[i] << (i*nbits);
@@ -69,7 +75,7 @@ uint comprimir(uint a[], int nbits) {
 
 
 int main(){
-  mask = mask(2);
+  int m = mask(2);
   printf("mask: %d\n", mask);
 
 }
